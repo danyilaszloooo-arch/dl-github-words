95884c35fb84a7e10484a379601702b61e0e0d1b
danyilaszloooo-arch <danyilaszloooo@gmail.com>
2026-08-27 20:36:27 +0200

    added save to local service button

--- src/app/components/save/save.ts
+++ src/app/components/save/save.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { WordService } from '../../services/word-service';
 
 @Component({
   selector: 'app-save',
@@ -6,4 +7,10 @@ import { Component } from '@angular/core';
   templateUrl: './save.html',
   styleUrl: './save.css',
 })
-export class Save {}
+export class Save {
+  wordService = inject(WordService)
+  words = this.wordService.words
+  protected saveToLocalStorage(): void {
+    localStorage.setItem('words', JSON.stringify(this.words()))
+  }
+}
