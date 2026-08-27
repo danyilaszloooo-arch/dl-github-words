5c09ae5c6e06fcce5c881110d8fe55a7c97c4ef8
danyilaszloooo-arch <danyilaszloooo@gmail.com>
2026-08-27 19:17:45 +0200

    handed words to table from api service

--- src/app/components/table/table.ts
+++ src/app/components/table/table.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
+import { Api } from '../../services/api';
 
 @Component({
   selector: 'app-table',
@@ -6,4 +7,12 @@ import { Component } from '@angular/core';
   templateUrl: './table.html',
   styleUrl: './table.css',
 })
-export class Table {}
+export class Table {
+  apiService = inject(Api)
+  words = signal<string[]>([])
+
+  async ngOnInit() {
+    const data = await this.apiService.getWords()
+    this.words.set(data)
+  }
+}
