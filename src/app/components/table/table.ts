b4d0d31d76b1f3e9ed75081aba9cc0333de54322
danyilaszloooo-arch <danyilaszloooo@gmail.com>
2026-08-27 19:22:17 +0200

    words broken up into chunks

--- src/app/components/table/table.ts
+++ src/app/components/table/table.ts
@@ -10,9 +10,23 @@ import { Api } from '../../services/api';
 export class Table {
   apiService = inject(Api)
   words = signal<string[]>([])
+  chunks = signal<string[][]>([])
+  cellsInRow: number = 4
+  rows: number = 1
 
   async ngOnInit() {
     const data = await this.apiService.getWords()
     this.words.set(data)
+    let spare = this.words().length % 4
+    if (spare > 1) spare = 1
+    this.rows = Math.floor(this.words().length / 4) + spare
+    this.chunks.set(this.chunkArray(this.words(), this.cellsInRow))
+  }
+  private chunkArray<T>(array: T[], chunkSize: number): T[][] {
+    let chunks: T[][] = []
+    for (let i = 0; i < array.length; i += chunkSize) {
+      chunks.push(array.slice(i, i + chunkSize))
+    }
+    return chunks
   }
 }
