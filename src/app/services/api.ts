9bbc6ac6e03d5cea36234e0cde346a6b7caeb693
danyilaszloooo-arch <danyilaszloooo@gmail.com>
2026-08-27 19:05:29 +0200

    added getWords method to api service

--- src/app/services/api.ts
+++ src/app/services/api.ts
@@ -3,4 +3,9 @@ import { Injectable } from '@angular/core';
 @Injectable({
   providedIn: 'root',
 })
-export class Api {}
+export class Api {
+  public async getWords(): Promise<string[]> {
+    const response = await fetch('https://api.siposm.hu/word')
+    return response.json()
+  }
+}
